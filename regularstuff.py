3afcdf8aad3f76b7570dda1257c9f58e3f9f8fdd
Gene Essel <eugene2essel@gmail.com>
2018-07-08 21:51:52 -0400

    finished zen work

---
+++ regularstuff.py
@@ -0,0 +1,20 @@
+import re
+
+zen = """Although never is 
+often better than
+*right* now.
+If the implementation
+is hard to explain,
+it's a bad idea.
+If the implementation
+is easy to explain,
+it may be a good
+idea. Namespaces
+area one honking
+great idea -- let's 
+do more of those!
+"""
+
+m = re.findall("^If", zen, re.MULTILINE)
+
+print(m)
